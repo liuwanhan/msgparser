770bc3515f4b2c697cc4b76e5611bcd84f87bbb3
shijinwei <535650995@qq.com>
2019-04-04 17:09:15 +0800

    second commit

---
+++ NumberGenerator/src/main/java/com/yiibai/App.java
@@ -0,0 +1,64 @@
+package com.yiibai;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import org.simplejavamail.*;
+
+import com.auxilii.msgparser.Message;
+import com.auxilii.msgparser.MsgParser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World1!" );
+        
+        MsgParser msgp = new MsgParser();
+        try {
+			   Message msg = msgp.parseMsg("D:\\data\\a.msg");
+			   
+			   String fromEmail = msg.getFromEmail();
+			   String fromName = msg.getFromName();
+			   String subject = msg.getSubject();
+			   String body = msg.getConvertedBodyHTML();
+			   
+			   System.out.println(fromEmail );
+			   System.out.println(fromName );
+			   System.out.println(subject );
+			   //System.out.println(body );
+			   
+			   File file =new File("D:\\data\\a.html");
+			   
+			   if(!file.exists()){
+			       file.createNewFile();
+			      }
+			   
+			   
+			   OutputStreamWriter write = new OutputStreamWriter(new FileOutputStream(file)); 
+			   BufferedWriter writer=new BufferedWriter(write); 
+			   writer.write(body);
+			   writer.close(); 
+			  
+			   System.out.println("Done");
+			   
+			   
+			   
+		} catch (UnsupportedOperationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+    }
+}
